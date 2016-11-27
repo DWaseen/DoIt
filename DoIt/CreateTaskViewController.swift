f0e5afecde830d9afbf1390aea2dc44810de0dfb
Dan Waseen <dan.waseen1@gmail.com>
2016-11-26 21:03:16 -0600

    Working version with task add page .. no Add button

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Dan Waseen on 11/25/16.
+//  Copyright © 2016 RevWon. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Dan Waseen on 11/24/16.
+//  Copyright © 2016 RevWon. All rights reserved.
+//
+
+import Foundation
+
+class Task
+{
+        var name = ""
+        var important = false
+    
+}
